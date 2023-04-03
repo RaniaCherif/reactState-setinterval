e2d7b0f2bc72b52c81475a6571ef76a08aaeae9f
rania <contactraniacherif@gmail.com>
2023-04-03 10:53:27 +0100

    added: skills

--- src/Profile.js
+++ src/Profile.js
@@ -34,23 +34,55 @@ class Profile extends React.Component {
   render() {
     return (
       <div className="card-container">
-        <div className="skills">
-          {this.state.shows ? (
-            <>
-              <span class="pro">PRO</span>
-              <h2>Hello i'm : {this.state.Person.fullName}</h2>
-              <h3>My bio is : {this.state.Person.bio}</h3>
-              <h style={{ color: "bisque" }}>
-                My last profession is : {this.state.Person.profession}
-              </h>
-              <img classeName="round" src={this.state.Person.imgSrc}></img>
-              <h3>Time:{this.state.mountingTime}</h3>
-            </>
-          ) : null}
-          <div class="buttons" onClick={this.change}>
-            <button class="primary">Profile</button>
-            <button class="primary ghost">Follow Me</button>
-          </div>
+        {this.state.shows ? (
+          <>
+            <span class="pro">PRO</span>
+            <h2>Hello i'm : {this.state.Person.fullName}</h2>
+            <h3>My bio is : {this.state.Person.bio}</h3>
+            <h3 style={{ color: "bisque" }}>
+              My last profession is : {this.state.Person.profession}
+            </h3>
+            <footer>
+              <p>
+                Created with <i class="fa fa-heart"></i> by
+                <a target="_blank" href="https://florin-pop.com">
+                  Florin Pop
+                </a>
+                - Read how I created this
+                <a
+                  target="_blank"
+                  href="https://florin-pop.com/blog/2019/04/profile-card-design"
+                >
+                  here
+                </a>
+                - Design made by
+                <a
+                  target="_blank"
+                  href="https://dribbble.com/shots/6276930-Profile-Card-UI-Design"
+                >
+                  Ildiesign
+                </a>
+              </p>
+            </footer>
+            <img classeName="round" src={this.state.Person.imgSrc}></img>
+            <h3>Time:{this.state.mountingTime}</h3>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>UI / UX</li>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul>
+            </div>
+          </>
+        ) : null}
+        <div class="buttons" onClick={this.change}>
+          <button class="primary">Profile</button>
+          <button class="primary ghost">Follow Me</button>
         </div>
       </div>
     );
